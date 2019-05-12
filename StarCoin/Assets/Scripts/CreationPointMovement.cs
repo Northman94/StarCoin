2b8c5320bb7204bcaa5ff36abc04aad7e37ef0a2
Zhanat Darmenov <zhandarm2000@gmail.com>
2019-05-12 16:50:57 +0300

    Creation Point Movement script + Object creation and Destruction script

---
+++ StarCoin/Assets/Scripts/CreationPointMovement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreationPointMovement : MonoBehaviour 
+{
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(Random.Range(-4.5f, 5f), 7f, 7f); // ( X ) / Y / Z
+    }
+}
---
+++ StarCoin/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour 
+{
+    [SerializeField]
+    private Transform _creationPoint = null;
+    [SerializeField]
+    private GameObject _heartPickUp = null;
+    [SerializeField]
+    private GameObject _bombPickUp = null;
+
+    private GameObject newObjInstance;
+
+    private float randomValue;
+
+    [SerializeField]
+    private float objLifeTime = 5f;
+
+
+    private void Start()
+    {
+        InvokeRepeating("Create", 1f, Random.Range(1f, 3f));
+    }
+
+
+    public void Create()
+    {
+        randomValue = Random.Range(1f, 10f);
+        // Heart or Bomb
+        if (randomValue > 4)
+        {
+            newObjInstance = Instantiate(_heartPickUp, _creationPoint.position, _creationPoint.rotation);
+        }
+        else
+        {
+            newObjInstance = Instantiate(_bombPickUp, _creationPoint.position, _creationPoint.rotation);
+        }
+
+        DestroyObj();
+    }
+
+    public void DestroyObj()
+    {
+        Destroy(newObjInstance, objLifeTime);
+    }
+}
