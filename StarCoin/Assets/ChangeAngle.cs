254f797e8b6839e361dc2f1ab56b92648742ac31
Zhanat Darmenov <zhandarm2000@gmail.com>
2019-05-12 19:44:51 +0300

    Platform rotation by mouse

---
+++ StarCoin/Assets/ChangeAngle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAngle : MonoBehaviour 
+{
+    float rotationSpeed = 100f;
+
+    private void OnMouseDrag()
+    {
+        float rotate_Y = Input.GetAxis("Mouse Y") * rotationSpeed * Mathf.Deg2Rad;
+
+        transform.Rotate(Vector3.forward, rotate_Y);
+    }
+
+
+
+
+    #region Movement
+    /*
+    private float distanse = 10f;
+
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanse);
+
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.position = objPosition;
+    }
+    */
+    #endregion
+
+}
